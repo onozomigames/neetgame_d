f38d11b188baba39c64a5af1b8ee97ed76af1051
teraoka-nozomi <onozomigames@gmail.com>
2025-06-10 16:02:30 +0900

    Utage

---
+++ Assets/Utage/Sample/Scripts/TipsMasterDetail/SampleUiTipsMasterDetailCategoryButton.cs
@@ -0,0 +1,51 @@
+﻿// UTAGE: Unity Text Adventure Game Engine (c) Ryohei Tokimura
+using UnityEngine;
+using UnityEngine.UI;
+using Utage;
+using UtageExtensions;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.Events;
+
+
+namespace Utage
+{
+
+	// TIPSのマスターディテールタイプの各カテゴリ名のボタン
+	public class SampleUiTipsMasterDetailCategoryButton : MonoBehaviour
+	{
+		protected SampleUiTipsMasterDetail MasterDetail => this.GetComponentCacheInParent(ref masterDetail);
+		SampleUiTipsMasterDetail masterDetail;
+
+		//カテゴリID
+		[SerializeField] protected string categoryId;
+		[SerializeField] protected GameObject newIcon;
+
+		public UnityEvent OnInit => onInit;
+		[SerializeField] UnityEvent onInit = new();
+
+		public SampleUiTipsMasterDetail.TipsCategoryInfo CategoryInfo { get; private set; }
+		
+		public virtual void Init()
+		{
+			CategoryInfo = MasterDetail.TipsCategories.Find(x=>x.Id == categoryId);
+			if (CategoryInfo == null)
+			{
+				Debug.LogError($"CategoryInfo is null. categoryId = {categoryId}");
+			}
+			RefreshNew();
+			OnInit.Invoke();
+		}
+
+		//Newアイコンのオンオフを更新
+		public virtual void RefreshNew()
+		{
+			//Newアイコンのオンオフ
+			if (newIcon != null)
+			{
+				newIcon.SetActive(CategoryInfo.IsNew());
+			}
+		}
+	}
+}
